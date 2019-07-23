9e85d8bcfc8345994c62e601f1eddfc8486d5aab
Brett Hurst <bretthurst@outlook.com>
2019-07-23 15:48:36 -0700

    Base64 decoding and query parsing for username

--- functions/lowPriority.js
+++ functions/lowPriority.js
@@ -1,11 +1,28 @@
+import querystring from 'querystring'
+
 exports.handler = function (event, context, callback) {
+
+    console.log(`Event body: ${event.body}`)
+
+    let base64Body = new Buffer(event.body, 'base64')
+    console.log(`Buffer: ${base64Body}`)
+
+    let queryString = base64Body.toString('ascii')
+    console.log(`Decoded query string: ${queryString}`)
+
+    let queryObject = querystring.parse(queryString)
+    console.log(`Parsed Query object: ${queryObject}`)
+
+    let invokedByUser = queryObject.user_name
+    console.log(`Invoking by user: ${invokedByuser}`)
+
 
     const res = {
        response_type: "in_channel",
         text: "http://devhumor.com/content/uploads/images/July2019/mvp_bugs.png",
         attachments: [
             {
-                text: `lowPriority is a simple Netlify function created by @Brett Hurst for the Struggle Buddy slack app. You should make it a highPriority to create your own! :D`
+                text: `Hey ${invokedByUser}! lowPriority is a simple Netlify function created by @Brett Hurst for the Struggle Buddy slack app. You should make it a highPriority to create your own! :D`
             }
         ]
     }

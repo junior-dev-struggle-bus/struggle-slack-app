ac959b35d3380ab85bc6166ef9bbb9b30ca7ac08
Brett Hurst <bretthurst@outlook.com>
2019-07-23 13:51:48 -0700

    Stringify response/Go error)

--- functions/lowPriority.js
+++ functions/lowPriority.js
@@ -8,6 +8,6 @@ exports.handler = function (event, context, callback) {
     callback(null, {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
-        body: res
+        body: JSON.stringify(res)
     })
 }

a899b9387900250d3116eedab9048d7ca9077f31
Brett Hurst <bretthurst@outlook.com>
2019-07-23 15:23:54 -0700

    Revert to basic message without invoker username

--- functions/lowPriority.js
+++ functions/lowPriority.js
@@ -1,17 +1,11 @@
 exports.handler = function (event, context, callback) {
 
-    console.log(`Event body: ${event.body}`)
-
-    const invokedByUser = event.body.user_name
-
-    console.log(`User name var: ${invokedByUser}`)
-
     const res = {
         response_type: "in_channel",
         text: "http://devhumor.com/content/uploads/images/July2019/mvp_bugs.png",
         attachments: [
             {
-                text: `Hey ${invokedByUser}! lowPriority is a simple Netlify function created by @Brett Hurst for the Struggle Buddy slack app. You should make it a highPriority to create your own! :D`
+                text: `lowPriority is a simple Netlify function created by @Brett Hurst for the Struggle Buddy slack app. You should make it a highPriority to create your own! :D`
             }
         ]
     }

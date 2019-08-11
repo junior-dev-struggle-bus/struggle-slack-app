c5ea7413a9cd38ffd150cdac658b28289d8a69de
Lisa Kim <lisakeem@outlook.com>
2019-08-11 01:03:00 -0700

    Implement feature: provide link to automated invite to wrecking-ball
    - Parse request body
    - Create structs for slack message payload metadata
    - Register function into slack-app-registry.json

--- inviteme/main.go
+++ inviteme/main.go
@@ -1,29 +1,100 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/gorilla/schema"
+	"net/http"
+	"net/url"
+)
+
+const (
+	inviteLink    string = "https://jdsb-test-ground-slack-invite.herokuapp.com/"
+	imageLink     string = "http://giphygifs.s3.amazonaws.com/media/5q7XU3MurT0pW/giphy.gif"
+	imageText     string = "I came in like a wrecking ball"
+	imageAltText  string = "Picture of a cat wrecking neatly stacked books"
+	linkMsg       string = "Click on the link below to get an invite to *jdsb-wrecking-ball* workspace: "
+	myJdsbSlackID        = "UGAA0BJ4R"
+)
+
+var (
+	decoder = schema.NewDecoder()
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	fmt.Println("lisa was here")
-	fmt.Println("Body--", request.Body)
-	fmt.Println("Headers--", request.Headers)
-	fmt.Println("HTTPMethod--", request.HTTPMethod)
-	fmt.Println("IsBase64Encoded--", request.IsBase64Encoded)
-	fmt.Println("MultiValueHeaders--", request.MultiValueHeaders)
-	fmt.Println("MultiValueQueryStringParameters--", request.MultiValueQueryStringParameters)
-	fmt.Println("Path--", request.Path)
-	fmt.Println("PathParameters--", request.PathParameters)
-	fmt.Println("QueryStringParameters--", request.QueryStringParameters)
-	fmt.Println("RequestContext--", request.RequestContext)
-	fmt.Println("Resource--   ", request.Resource)
-	fmt.Println("StageVariables--", request.StageVariables)
+	parsedBody, err := url.ParseQuery(request.Body)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       fmt.Sprintf("%d: %s", http.StatusBadRequest, ErrParseRequestBody.Error()),
+		}, ErrParseRequestBody
+	}
+
+	var receivedData SlackRequestBody
+	err = decoder.Decode(&receivedData, parsedBody)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       fmt.Sprintf("%d: %s", http.StatusInternalServerError, ErrDecodingParsedQuery.Error()),
+		}, ErrDecodingParsedQuery
+	}
+
+	blockSection := SlackBlockSection{
+		Type: SBlockSection,
+		Text: SlackTypeText{
+			Type: STypeMarkDown,
+			Text: fmt.Sprintf("Hello <@%s>, fellow 🤓! \n%s\n\n🔗 %s 🔗", receivedData.UserID, linkMsg, inviteLink),
+		},
+	}
+
+	blockDivider := SlackBlockDivider{Type: SBlockDivider}
+
+	blockImage := SlackBlockImage{
+		Type: STypeImage,
+		Title: SlackTypeText{
+			Type: STypePlain,
+			Text: imageText,
+		},
+		ImageURL: imageLink,
+		AltText:  imageAltText,
+	}
+
+	blockContext := SlackBlockContext{
+		Type: SBlockContext,
+		Elements: []SlackTypeText{
+			{
+				Type: STypeMarkDown,
+				Text: fmt.Sprintf("*Function by:* <@%s>", myJdsbSlackID),
+			},
+		},
+	}
+
+	slackPayload := SlackMessagePayload{
+		ResponseType: STypeResponse,
+		ChannelName:  receivedData.ChannelName,
+		Token:        receivedData.Token,
+		Blocks: []interface{}{
+			blockSection,
+			blockDivider,
+			blockImage,
+			blockContext,
+		},
+	}
+
+	slackPayloadByte, err := json.Marshal(slackPayload)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       fmt.Sprintf("%d: %s", http.StatusInternalServerError, ErrJSONMarshal.Error()),
+		}, ErrJSONMarshal
+	}
 
 	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello, World",
+		Headers:    JSONHeader,
+		StatusCode: http.StatusOK,
+		Body:       string(slackPayloadByte),
 	}, nil
 }
 
---
+++ inviteme/slackapi.go
@@ -0,0 +1,82 @@
+package main
+
+import "errors"
+
+// SlackRequestBody contains events.APIGatewayProxyRequest.Body received from slack
+type SlackRequestBody struct {
+	Token        string   `schema:"token"`
+	TeamID       string   `schema:"team_id"`
+	TeamDomain   string   `schema:"team_domain"`
+	ChannelID    string   `schema:"channel_id"`
+	ChannelName  string   `schema:"channel_name"`
+	UserID       string   `schema:"user_id"`
+	UserName     string   `schema:"user_name"`
+	Command      string   `schema:"command"`
+	CommandTexts []string `schema:"text"`
+	ResponseURL  string   `schema:"response_url"`
+	TriggerID    string   `schema:"trigger_id"`
+}
+
+// SlackMessagePayload contains metadata to be JSONed for slack API to publish message
+// Message Payload: https://api.slack.com/reference/messaging/payload
+// Block Types: https://api.slack.com/reference/messaging/blocks
+type SlackMessagePayload struct {
+	ResponseType string        `json:"response_type"`
+	ChannelName  string        `json:"channel"`
+	Token        string        `json:"token"`
+	Blocks       []interface{} `json:"blocks"`
+}
+
+// SlackTypeText used for nesting objects inside a block
+type SlackTypeText struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
+
+// SlackBlockSection is a block that will display simplest of texts
+type SlackBlockSection struct {
+	Type string        `json:"type"`
+	Text SlackTypeText `json:"text"`
+}
+
+// SlackBlockDivider is a content divider to split up different blocks inside a message
+type SlackBlockDivider struct {
+	Type string `json:"type"`
+}
+
+// SlackBlockImage is a block that displays images and its associated texts
+type SlackBlockImage struct {
+	Type     string        `json:"type"`
+	Title    SlackTypeText `json:"title"`
+	ImageURL string        `json:"image_url"`
+	AltText  string        `json:"alt_text"`
+}
+
+// SlackBlockContext is a block that displays message context
+type SlackBlockContext struct {
+	Type     string          `json:"type"`
+	Elements []SlackTypeText `json:"elements"`
+}
+
+// consts related to slack: header types and creating message layouts
+const (
+	HeaderContentType     = "Content-type"
+	HeaderContentTypeJSON = "application/json"
+	SBlockSection         = "section"
+	SBlockDivider         = "divider"
+	SBlockImage           = "image"
+	SBlockContext         = "context"
+	STypeMarkDown         = "mrkdwn"
+	STypePlain            = "plain_text"
+	STypeResponse         = "in_channel"
+	STypeImage            = "image"
+)
+
+// vars related to slack: headers and error messages
+var (
+	JSONHeader             = map[string]string{HeaderContentType: HeaderContentTypeJSON}
+	ErrEmptyRequestBody    = errors.New("APIGatewayProxyRequest.Body empty")
+	ErrParseRequestBody    = errors.New("unable to parse APIGatewayProxyRequest.Body")
+	ErrDecodingParsedQuery = errors.New("unable to decode parsed query into a struct")
+	ErrJSONMarshal         = errors.New("unable to JSON encode for response")
+)

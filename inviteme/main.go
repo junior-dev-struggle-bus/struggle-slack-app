0e4acf200a073263c7176e0ba04c82076bda6703
Lisa Kim <lisakeem@outlook.com>
2019-08-11 00:55:54 -0700

    Test with logs for APIGatewayProxyRequest object

--- inviteme/main.go
+++ inviteme/main.go
@@ -8,7 +8,19 @@ import (
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	fmt.Println("lisa was here")
-	fmt.Println(request)
+	fmt.Println("Body--", request.Body)
+	fmt.Println("Headers--", request.Headers)
+	fmt.Println("HTTPMethod--", request.HTTPMethod)
+	fmt.Println("IsBase64Encoded--", request.IsBase64Encoded)
+	fmt.Println("MultiValueHeaders--", request.MultiValueHeaders)
+	fmt.Println("MultiValueQueryStringParameters--", request.MultiValueQueryStringParameters)
+	fmt.Println("Path--", request.Path)
+	fmt.Println("PathParameters--", request.PathParameters)
+	fmt.Println("QueryStringParameters--", request.QueryStringParameters)
+	fmt.Println("RequestContext--", request.RequestContext)
+	fmt.Println("Resource--   ", request.Resource)
+	fmt.Println("StageVariables--", request.StageVariables)
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World",
@@ -16,7 +28,6 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 }
 
 func main() {
-	fmt.Println("starting main")
 	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }

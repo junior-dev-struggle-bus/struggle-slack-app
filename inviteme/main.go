de7cf85454d4162e723da5cebd74d3300a18f450
Lisa Kim <lisakeem@outlook.com>
2019-08-10 22:42:07 -0700

    Test aws-lambda-go netlify's example

--- inviteme/main.go
+++ inviteme/main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"What is your name?"`
-	Age int     `json:"How old are you?"`
-}
-
-type MyResponse struct {
-	Message string `json:"Answer:"`
-}
-
-func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, World",
+	}, nil
 }
 
 func main() {
-	lambda.Start(HandleLambdaEvent)
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
 }

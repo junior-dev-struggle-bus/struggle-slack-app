1d06cc813e1e94740396dc34cb289301f2d39e08
Lisa Kim <lisakeem@outlook.com>
2019-08-11 00:36:14 -0700

    Test with logs

--- inviteme/main.go
+++ inviteme/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	fmt.Println("lisa was here")
+	fmt.Println(request)
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World",
@@ -13,6 +16,7 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 }
 
 func main() {
+	fmt.Println("starting main")
 	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }

9742bcbfe2e79ef97c57d71048da07b90adc082c
Lisa Kim <lisakeem@outlook.com>
2019-08-10 22:28:07 -0700

    Testing another aws-lambda-go example

--- inviteme/main.go
+++ inviteme/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
-	"context"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type MyEvent struct {
-	Name string `json:"Lisa"`
+	Name string `json:"What is your name?"`
+	Age int     `json:"How old are you?"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name ), nil
+type MyResponse struct {
+	Message string `json:"Answer:"`
+}
+
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(HandleLambdaEvent)
 }
